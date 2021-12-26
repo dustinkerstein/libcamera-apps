820ab70b0c86a7ad51ddd9f0041fe8156b537173
Dustin Kerstein <dustin.kerstein@gmail.com>
2021-12-26 10:18:43 -0800

    Padding test

--- output/control_output.cpp
+++ output/control_output.cpp
@@ -35,14 +35,21 @@ void ControlOutput::WriteOut()
 	fp_timestamps_ = nullptr;
 	if (Control::enableBuffer) 
 	{
-		bool snowed = false;
+		bool padded = false;
 		while(framesWritten_ < framesBuffered_)
 		{
-			if (Control::mode == 3 && framesWritten_ == 0 && !snowed) {
-				std::cerr << "LIBCAMERA: Generating Snow" << std::endl;
-				std::system("gst-launch-1.0 videotestsrc pattern=snow num-buffers=10 ! video/x-raw,width=4096,height=3040,framerate=30/1 ! filesink location=~/pipe");
-				std::cerr << "LIBCAMERA: Snow Generation Complete" << std::endl;
-				snowed = true;
+			if (Control::mode == 3 && framesWritten_ == 0 && !padded) {
+				for (int i = 0; i < 11; i++) { // HACK
+					if (fwrite(buf_[framesWritten_], 18677760, 1, fp_) != 1)
+						std::cerr << "LIBCAMERA: failed to write output bytes" << std::endl;
+					else
+					{
+						std::cerr << "LIBCAMERA: PADDING FRAMES" << std::endl;
+						framesWritten_++;
+					}
+				}
+				framesWritten_ = 0;
+				padded = true;
 			} else if (fwrite(buf_[framesWritten_], 18677760, 1, fp_) != 1)
 				std::cerr << "LIBCAMERA: failed to write output bytes" << std::endl;
 			else {
