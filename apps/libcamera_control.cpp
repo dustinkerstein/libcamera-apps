df70650d367cbf35a85a71c2f4a5c2f155851773
Dustin Kerstein <dustin.kerstein@gmail.com>
2022-01-04 12:52:54 -0800

    Debug for exit

--- apps/libcamera_control.cpp
+++ apps/libcamera_control.cpp
@@ -271,6 +271,7 @@ int main(int argc, char *argv[])
 			}
 			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
+		std::cerr << "LIBCAMERA: EXITING. SHOULD NOT BE HERE" << std::endl;
 	}
 	catch (std::exception const &e)
 	{
